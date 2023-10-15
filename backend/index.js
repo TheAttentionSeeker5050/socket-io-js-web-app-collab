b4e071aa8ea2c67bf35b0cc1f8f055f2f0f16a20
nicolas <nicolas.castellano.6@hotmail.com>
2023-10-15 17:31:03 -0300

    before adding db connection

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,26 @@
+
+// ---------------------------------------------------------------
+// express server
+const express = require('express');
+const expressApp = express();
+const expressPort = 8080;
+
+
+expressApp.get('/', (req, res) => {
+    res.send('<h1>get response for the chat app</h1>');
+});
+
+
+expressApp.listen(expressPort, () => {
+    console.log(`listening on port ${expressPort}`);
+});
+
+// ---------------------------------------------------------------
+// socket.io server
+
+
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const express = require('express');
-const app = express();
-const port = 8080;
-
-
-app.get('/', (req, res) => {
-    res.send('<h1>get response for the chat app</h1>');
-    // res.send('<h1>get response for the chat app </h1><p>some other paragraph</p>');
-});
-
-
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
-
-// the following is the code for socket.io
 
 const socketIOPort = 8081;
 const httpServer = createServer();
@@ -36,3 +41,12 @@ io.on('connection', (socket) => {
 httpServer.listen(socketIOPort, () => {
     console.log(`socket.io listening on port ${socketIOPort}`);
 });
+
+
+// postgressql drivers ----------------------------------------------------
+
+// for the express server
+const exppgdriver = require('pg-promise')();
+// this is the connection to the database through the express server
+const expdb = exppgdriver('postgres://postgres:mysecretpassword@chat-app-database:5432/postgres');
+
