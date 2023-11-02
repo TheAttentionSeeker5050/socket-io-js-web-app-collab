6a1505d44fbf85146a2a936347876cb111ac26e6
nicolas <nicolas.castellano.6@hotmail.com>
2023-11-02 17:39:24 -0300

    created login and register route

---
+++ backend/routes.js
@@ -0,0 +1,23 @@
+// here we create a route module, 
+// user makes requests to the server, server has defined routes
+// and logics mapped in here
+
+const express = require("express");
+const router = express.Router();
+
+// the routes here, mapped as address string, function(request object, response object)
+
+// the login route
+router.post("/login", function(req, res) {
+    res.status(200).json({
+        "message": "Login Successful",
+    });
+});
+
+// the register route
+router.post("/register", function(req, res) {
+    res.status(200).json({
+        "message": "Register Successful",
+    });
+});
+
--- backend/socket/mainSocketController.js
+++ backend/socket/mainSocketController.js
@@ -4,8 +4,10 @@ const { Server } = require("socket.io");
 
 const socketIOPort = 8081;
 
+// in general, this is an HTTP server that will be used by socket.io, among other things
 const httpServer = createServer();
 
+// this will be used to add new socket.io events
 const socketIOServer = new Server(httpServer, {
     cors: {
         origin: "http://localhost:3000",
@@ -13,8 +15,11 @@ const socketIOServer = new Server(httpServer, {
     }
 });
 
+// this is the main socket.io event listener, it scans for new connections
+// when a new connection is made, it will log a message to the console
 socketIOServer.on('connection', (socket) => {
     console.log('a user connected');
+    // when a user disconnects, it will log a message to the console
     socket.on('disconnect', () => {
         console.log('user disconnected');
     })
